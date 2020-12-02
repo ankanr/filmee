5e9b772aa9bf39ce2750d33cf27bcfe20322e423
ankanr <zeekozk9@gmail.com>
2020-12-02 18:21:12 +0530

    pre auth commit

--- client/src/Home.js
+++ client/src/Home.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import SearchMovies from './SearchMovie';
 
+const oktaApiKey = '00IdaKLeETjzV7wmqfn1Ch3f7jEiWrCzxgFrht24JA';
+
 export default function Home() {
   return (
     <>
